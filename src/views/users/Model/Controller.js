0bd09daf58c444e8a3e5ec8f75635946afe29753
Quoc Anh Truong <teomoney1999@gmail.com>
2022-01-17 15:39:16 +0700

    add

--- src/views/users/Model/Controller.js
+++ src/views/users/Model/Controller.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
+import useHttp from 'src/hooks/use-http'
+import { getSingle } from 'src/helpers/request-helper'
 import { reqHandler } from 'src/helpers/http-helper'
 import Config from '../../../configs/config'
 import View from './View'
@@ -18,29 +20,43 @@ const Controller = (props) => {
 
   const goBackHandler = () => history.goBack()
 
+  const { req, data, error } = useHttp(getSingle)
+
   useEffect(() => {
+    console.log('useEffect')
     let isUnmount = true
 
     ;(async () => {
-      try {
-        const resp = await reqHandler({
-          url: `${url}/${apiPrefix}/${apiEndpoint}/${param.userId}`,
-        })
-        if (!isUnmount) {
-          console.log('Component unmounted!')
-          return
-        }
-        setUser(resp)
-      } catch (error) {
-        console.log('error', error)
+      console.log('LOAD USER!')
+      await req({ endpoint: apiEndpoint, id: param.userId })
+
+      console.log('resp', data)
+      if (!isUnmount) {
+        console.log('Component unmounted!')
         return
       }
+      setUser(data)
+
+      // try {
+      //   const resp = await reqHandler({
+      //     url: `${url}/${apiPrefix}/${apiEndpoint}/${param.userId}`,
+      //   })
+      //   if (!isUnmount) {
+      //     console.log('Component unmounted!')
+      //     return
+      //   }
+      //   setUser(resp)
+      // } catch (error) {
+      //   console.log('error', error)
+      //   return
+      // }
     })()
 
     return () => {
       isUnmount = false
     }
-  }, [reqHandler, url, apiPrefix, apiEndpoint])
+  }, [req, url, apiPrefix, apiEndpoint, user])
+
   return <View data={user} onGoBack={goBackHandler} />
 }
 
